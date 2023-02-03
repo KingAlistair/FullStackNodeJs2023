374d5390ffeeb8d59d7b6fa464490beea9f40f77
Dániel Szabó <kingalistairlol@gmail.com>
2023-02-03 10:35:06 +0100

    Created the variables file

---
+++ 00_Miscellaneous/variables.js
@@ -0,0 +1,17 @@
+//Const
+
+const me = {};
+// key: value
+// key-value pair
+me.name = "Johannes";
+
+console.log(me);
+
+const hobbies = ["Football", "Swimming"];
+hobbies.push("Cooking");
+
+console.log(hobbies);
+
+const cookingSkill = `Cooking skill: ${2+2}`;
+
+console.log(cookingSkill);
